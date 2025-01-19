022b59213f82e6227ae294652ac4182164a469e7
Alessa Castillo <alessacastillo2001@gmail.com>
2025-01-18 20:27:04 -0600

    Process Env

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const { v4: uuidv4 } = require('uuid'); // Import uuidv4
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT ||3001;
 
 app.use(cors({ origin: 'http://localhost:3000' }));
 
